ff4b21b36d0fcd4f67d152082579569d1e2c6c55
NgocAnhNLU <22130013@st.hcmuaf.edi.vn>
2023-02-18 11:06:39 +0700

    btao treb VLog

---
+++ DeoGitHub/If_b.java
@@ -0,0 +1,20 @@
+package DeoGitHub;
+
+import java.util.Scanner;
+
+public class If_b {
+    public static void main(String[] args) {
+        boolean isRaining;
+        Scanner input =new Scanner(System.in);
+        System.out.print("input");
+        isRaining =input.nextBoolean();
+        if (isRaining==true) {
+            System.out.println("Remember to bring an umbrella");
+            
+        }
+        else{
+            System.out.println("Enjoy the sunshine");
+
+        }
+    }
+}
--- week1/Ex5.java
+++ week1/Ex5.java
@@ -13,13 +13,7 @@ public class Ex5 {
 		second-=hour*3600;
 		minute=second/60;
 		second-=minute*60;
-<<<<<<< HEAD:week1/Ex5.java
 		System.out.printf("%d:%d':%d\"",hour,minute,second);
-=======
-		System.out.printf("%dh:%d':%d\"",hour,minute,second);
-<<<<<<< HEAD
-		System.out.printf("%dh:%d':%d\"",hour,minute,second);
->>>>>>> 7d7ef71bc5755ec7d02504145a2fbefb68a41d81:week1/Ex5.java
 =======
 //		System.out.printf("%dh:%d':%d\"",hour,minute,second);
 >>>>>>> 778dcae03cddb688bd02b9f3b4f94d8da40745e6
