7d7ef71bc5755ec7d02504145a2fbefb68a41d81
Ngọc Anh <nguyenhuungocanh@Tuyn-Laptop.local>
2023-02-18 09:13:29 +0700

    commen

--- week1/Ex5.java
+++ week1/Ex5.java
@@ -1,10 +1,11 @@
 package week1;
 
 import java.util.Scanner;
-
+//commen de test git hub
 public class Ex5 {
 	public static void main(String[] args) {
 		Scanner input =new Scanner(System.in);
+		//khai bao doi tuong
 		int hour, minute,second;
 		System.out.print("enter Second:");
 		second=input.nextInt();
@@ -12,7 +13,8 @@ public class Ex5 {
 		second-=hour*3600;
 		minute=second/60;
 		second-=minute*60;
-		System.out.printf("%d:%d':%d\"",hour,minute,second);
+		System.out.printf("%dh:%d':%d\"",hour,minute,second);
+		System.out.printf("%dh:%d':%d\"",hour,minute,second);
 		
 	}
 
