61596574c9ea59eca420c4be3d5549cb7362f542
NgocAnhNLU <22130013@st.hcmuaf.edi.vn>
2023-02-19 23:53:03 +0700

    Delete GitDemo

---
+++ switchCaseJava/SwitchCaseJava_e.java
@@ -0,0 +1,11 @@
+package switchCaseJava;
+
+import java.util.Scanner;
+
+public class SwitchCaseJava_e {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.print("");
+	}
+
+}
