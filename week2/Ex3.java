eb2316ab3961d497cbac29b1cee17c1a7a5066d2
Nguyễn Hữu Ngọc Anh <125566442+NgocAnhNLU@users.noreply.github.com>
2023-02-22 10:24:47 +0700

    commentEx3

--- week2/Ex3.java
+++ week2/Ex3.java
@@ -10,16 +10,17 @@ public class Ex3 {
 		a=input.nextInt();
 		System.out.print("Input Number b: ");
 		b=input.nextInt();
-//		System.out.printf("phuong trinh %dx+%d=0",a,b);
+//		System.out.printf("phương trình %dx+%d=0",a,b);
 		giaiPTBac1(a, b);
 	}
+// Hàm nhận 2 giá trị a,b và giải Phương trình bậc nhất
 	public static void giaiPTBac1(int a, int b) {
 		if (a==0&&b!=0) 
-			System.out.printf("phuong trinh %dx+%d=0 vo nghiem",a,b);
+			System.out.printf("phương trình %dx+%d=0 vô nghiệm",a,b);
 		if (a==0&&b==0) 
-			System.out.printf("phuong trinh %dx+%d=0 vo so nghiem",a,b);
+			System.out.printf("phương trình %dx+%d=0 Vô số nghiệm",a,b);
 		if (a!=0) 
-			System.out.printf("%f la nghiem cua PT %dx+%d=0",(double)-b/a,a,b);
+			System.out.printf("%f là nghiệm của phương trình phương trình %dx+%d=0",(double)-b/a,a,b);
 	}
 
 }
